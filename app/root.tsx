5dbbfdfb03ad4a9e999a1b0f53f9e334524f480a
Guilherme Silva <guilherme.silva@rgsistemas.com.br>
2022-09-10 09:08:03 -0300

    chore: add tailwind to project

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -8,6 +8,10 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'Quaddro Appoitments',
@@ -16,7 +20,7 @@ export const meta: MetaFunction = () => ({
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <Meta />

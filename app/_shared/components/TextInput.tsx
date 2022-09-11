4757980262345923b01b54fceff2b87b9347b260
Guilherme Silva <guilherme.silva@rgsistemas.com.br>
2022-09-11 12:32:34 -0300

    chore: create base text input component

---
+++ app/_shared/components/TextInput.tsx
@@ -0,0 +1,30 @@
+import type { InputHTMLAttributes } from 'react';
+
+interface TextInputPros extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+export default function TextInput({
+  name,
+  label,
+  value,
+  ...rest
+}: TextInputPros) {
+  return (
+    <div className="flex flex-1 flex-col">
+      <label htmlFor={name} className="text-2md text-zinc-600">
+        {label}
+      </label>
+      <div className="w-full h-9 border rounded border-gray-300 bg-white overflow-hidden focus:border-blue-500">
+        <input
+          {...rest}
+          type="text"
+          name={name}
+          value={value}
+          className="w-full h-full px-2 text-zinc-600"
+        />
+      </div>
+    </div>
+  );
+}
